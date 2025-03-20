09ea5f637e748335b3a523805eb0a5c2c45c2ce0
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-03-20 13:04:15 +0100

    Se han adaptado las clases para la gestion de nif y excel

--- AppVehiculos/src/appvehiculos/ExcelManager.java
+++ AppVehiculos/src/appvehiculos/ExcelManager.java
@@ -11,23 +11,45 @@ import java.io.IOException;
  *
  * Esta clase es la encargada de leer y modificar el fichero Excel de datos usando
  * la libreria ApachePOI
- *  EL ORDEN DE LAS COLUMNAS NO VAN A CAMBAIR, LOS TIOS DE DATOS DE LAS COLUMNAS NO VAN A CAMBIAR.
- *  el LAS ORDENANZAS IGUAL, LOS TRAMOS PARA LOS TURISMOS SON IGUALES EN TODA ESPAÑA, NO DEBERIAN CAMBIAR
+ *  EL ORDEN DE LAS COLUMNAS NO VAN A CAMBAIR, LOS TIPOS DE DATOS DE LAS COLUMNAS NO VAN A CAMBIAR.
+ *  EN LAS ORDENANZAS IGUAL, LOS TRAMOS PARA LOS TURISMOS SON IGUALES EN TODA ESPAÑA, NO DEBERIAN CAMBIAR ENTRE MUNICIPIOS
  */
 public class ExcelManager {
-    XSSFWorkbook ExcelAgua;
-    public ExcelManager(){
+    XSSFWorkbook Excel;
+    String rute;
+    String fileName;
+    String suffix;
+    public ExcelManager(String rute, String fileName, String suffix){
+        this.rute = rute;
+        this.fileName = fileName;
+        this.suffix = suffix;
+        openExcel();
 
     }
-    public boolean openExcel(String rute){
+
+    ExcelManager() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public boolean closeExcel() throws FileNotFoundException {
+        if(Excel == null){
+            throw new FileNotFoundException("No se ha cargado el archivo excel, usa el metodo openExcel primero");
+        }
+        try{
+            Excel.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+    private boolean openExcel(){
         try {
-            FileInputStream fis = new FileInputStream(rute);
+            FileInputStream fis = new FileInputStream(this.rute + this.fileName + ".xlsx");
             XSSFWorkbook workbook = new XSSFWorkbook(fis);
-            this.ExcelAgua = workbook;
+            this.Excel = workbook;
             System.out.println("Archivo Excel cargado correctamente");
             return true;
         } catch (FileNotFoundException e) {
-            System.out.println("ERROR No se ha encontrado el archivo");
+            System.out.println("ERROR No se ha encontrado el archivo: "+ this.rute + this.fileName + ".xlsx");
             throw new RuntimeException(e);
         } catch (IOException e) {
             System.out.println("Se ha producido un error al intentar cargar el archivo");
@@ -35,56 +57,58 @@ public class ExcelManager {
         }
     }
     public int getRowsCount(int sheetNum){
-        XSSFSheet sheet =  ExcelAgua.getSheetAt(sheetNum);
+        XSSFSheet sheet =  Excel.getSheetAt(sheetNum);
         return sheet.getLastRowNum()+1;
     }
-    public boolean closeExcel(String rute) throws FileNotFoundException {
-        if(ExcelAgua == null){
-            throw new FileNotFoundException("No se ha cargado el archivo excel, usa el metodo openExcel primero");
-        }
-        try{
-            ExcelAgua.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return true;
-    }
+
 
     public String getCellValue(int sheetNumber, int rowNum, int columNum) throws FileNotFoundException {
-        if(ExcelAgua == null){
+        if(Excel == null){
             throw new FileNotFoundException("No se ha cargado el archivo excel, usa el metodo openExcel primero");
         }
         String result = "";
         // Obtener la hoja de trabajo (sheet) por su índice (por ejemplo, la primera hoja)
-        XSSFSheet sheet = ExcelAgua.getSheetAt(sheetNumber);
+        XSSFSheet sheet = Excel.getSheetAt(sheetNumber);
         XSSFRow row = sheet.getRow(rowNum);
         if(row == null){
+            System.out.println("Es quiiii");
             return "EMPTYROW";
         }
         boolean empty = true;
         for (int i = 0; i < row.getLastCellNum(); i++) {
-            if(row.getCell(i) !=null&& row.getCell(i).getStringCellValue()!=null&& row.getCell(i).getStringCellValue()!=""){
+            if(row.getCell(i) !=null&&row.getCell(i).getCellType().equals("STRING") && row.getCell(i).getStringCellValue()!=null&& row.getCell(i).getStringCellValue()!=""){
+
+                empty = false;
+                break;
+            }else{
                 empty = false;
                 break;
             }
         }
         if(empty){
+            System.out.println("Es quiiii");
             return "EMPTYROW";
         }
         XSSFCell cell = row.getCell(columNum);
         if(cell == null){
             return "";
         }
-        result = cell.getStringCellValue();
+        if(cell.getCellType().equals("STRING")){
+            result = cell.getStringCellValue();
+        }else{
+            if(cell == null ||cell.getRawValue() == null ||cell.getRawValue().isEmpty()){return "EMPTYROW";}
+            result = cell.getRawValue().toString();
+        }
+        
         return result;
     }
     public boolean setCellValue(int sheetNumber, int rowNum, int columNum, String newValue) throws IOException {
-        if(ExcelAgua == null){
+        if(Excel == null){
             throw new FileNotFoundException("No se ha cargado el archivo excel, usa el metodo openExcel primero");
         }
 
         // Obtener la hoja de trabajo (sheet) por su índice (por ejemplo, la primera hoja)
-        XSSFSheet sheet = ExcelAgua.getSheetAt(sheetNumber);
+        XSSFSheet sheet = Excel.getSheetAt(sheetNumber);
         XSSFRow row = sheet.getRow(rowNum);
         if(row == null){
             return false;
@@ -94,8 +118,8 @@ public class ExcelManager {
             return false;
         }
         cell.setCellValue(newValue);
-        FileOutputStream fos = new FileOutputStream("resources/SistemasAguaTrasEjecucionTest.xlsx");
-        ExcelAgua.write(fos);
+        FileOutputStream fos = new FileOutputStream(this.rute + this.fileName + this.suffix + ".xlsx");
+        Excel.write(fos);
         fos.close();
         return true;
     }
--- AppVehiculos/src/appvehiculos/NifNieValidator.java
+++ AppVehiculos/src/appvehiculos/NifNieValidator.java
@@ -1,4 +1,10 @@
 package appvehiculos;
+
+import POJOS.Contribuyente;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * @author Jose Antonio Lopez Perez
  *
@@ -14,96 +20,82 @@ package appvehiculos;
  * calculando el nif nie correcto y lo devuelve
  */
 public class NifNieValidator {
-    public NifNieValidator(){
+
+    /*
+    Retorna false si no tiene un formato valido/reparable y por tanto se debe generar un xml.
+    */
+    public static boolean check(String text){
+        boolean valid = true;
+        //Check if is not valid
+        if(text.length() == 9){
+            //check number positions
+            for (int i = 1; i < 8; i++) {
+                if(!Character.isDigit(text.charAt(i))){
+                    valid = false;
+                }
+            }
+            //check has control letter
+            if(!Character.isLetter(text.charAt(8))){
+                valid = false;
+            }
+        }else{
+            //TODO ¿Que pasa con los casos en elos que llegan los digitos correctos sin letra?
+            valid = false;
+        }
+        if(valid){
+            if(Character.isDigit(text.charAt(0))){
+                return checkNif(text);
+            }else{
+                return checkNie(text);
+            }
+        }else{
+            return false;
+        }
+    }
+    
+    private static boolean checkNie(String text) {
+        if(text.charAt(0) != 'X' && text.charAt(0) != 'Y' && text.charAt(0) != 'Z'){
+            return false;
+        }
+        String NIEchars = "XYZ";
+        String digits = NIEchars.indexOf(text.charAt(0)) + text.substring(1,8);
+
+        if(text.charAt(8) != getControlLetter(digits)){
+            //Fix nie sin avisar.
+            return fixNie(digits, text);
+        }
+        return true;
 
     }
-    public String checkNifNie(String code){
-        String type = getType(code);
-        if (type == "NIF"){
-            return checkNif(code);
-        }
-        if (type == "NIE"){
-            return checkNie(code);
-        }
-        return "NotValid";
-    }
-    public String getFixedNifNie(String code){
-        if(checkNifNie(code) == "NotValid"){
-            throw new IllegalArgumentException("Cant be fixed");
-        }
-        if(checkNifNie(code) == "correct"){
-            return code;
-        }
-        String type = getType(code);
-        if (type == "NIF"){
-            return fixNif(code);
-        }
-        if (type == "NIE"){
-            return fixNie(code);
-        }
-        return "null";
-    }
-
-    private String fixNie(String code) {
-        String digits = "";
-        switch (code.charAt(0)){
-            case 'X':
-                digits = "0" + code.substring(1,8);
-                break;
-            case 'Y':
-                digits = "1" + code.substring(1,8);
-                break;
-            case 'Z':
-                digits = "2" + code.substring(1,8);
-                break;
-        }
+    private static boolean fixNie(String digits, String brokenNie) {
         char controlLetter = getControlLetter(digits);
-        return digits+controlLetter;
+        String NIEchars = "XYZ";
+        String correctedNie = NIEchars.charAt(digits.charAt(0)) + digits.substring(1)+ controlLetter;
+        //TODO actualizar en el excel el nuevo valor del nif
+        
+        return true;
     }
 
-    private String fixNif(String code) {
-        String digits = code.substring(0,8);
-        char controlLetter = getControlLetter(digits);
-        return digits+controlLetter;
-    }
+    private static boolean checkNif(String text) {
 
-    private String checkNie(String code) {
-        if(code.charAt(0) != 'X' && code.charAt(0) != 'Y' && code.charAt(0) != 'Z'){
-            return "NotValid";
-        }
-        String digits = "";
-        switch (code.charAt(0)){
-            case 'X':
-                digits = "0" + code.substring(1,8);
-                break;
-            case 'Y':
-                digits = "1" + code.substring(1,8);
-                break;
-            case 'Z':
-                digits = "2" + code.substring(1,8);
-                break;
-        }
+        String digits = text.substring(0,8);
         char controlLetter = getControlLetter(digits);
 
-        if(code.charAt(8) != controlLetter){
-            return "broken";
+        if(text.charAt(8) != controlLetter){
+            return fixNif(digits, text);
         }
-        return "correct";
+        return true;
 
     }
-
-    private String checkNif(String code) {
-
-        String digits = code.substring(0,8);
+    private static boolean fixNif(String digits, String brokenNie) {
         char controlLetter = getControlLetter(digits);
-
-        if(code.charAt(8) != controlLetter){
-            return "broken";
-        }
-        return "correct";
-
+        //TODO actualizar en el excel el nuevo nif
+        
+        String correctedNif = digits+controlLetter;
+        return true;
     }
-    private char getControlLetter(String digits){
+
+    private static char getControlLetter(String digits){
         String controlString = "TRWAGMYFPDXBNJZSQVHLCKE";
         int intValue = Integer.valueOf(digits);
         int letterPos = intValue%23;
@@ -111,25 +103,34 @@ public class NifNieValidator {
         return controlLetter;
     }
 
-    private String getType(String code) {
-        if(code.length() == 9){
-            //check number positions
-            for (int i = 1; i < 8; i++) {
-                if(!Character.isDigit(code.charAt(i))){
-                    return "NotValid";
-                }
-            }
-            //check has control letter
-            if(!Character.isLetter(code.charAt(8))){
-                return "NotValid";
-            }
-            if(Character.isDigit(code.charAt(0))){
-                return "NIF";
-            }else{
-                return "NIE";
-            }
+    /**
+     Metodos dedicados a la generación del XML con el formato específico de nif y nie
+     */
+    public static boolean saveXML(String xmlData) throws IOException{
+        try (FileWriter writer = new FileWriter("resources/ErroresNifNie.xml", false)) {
+            
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+            writer.write("<Contribuyentes>\n");
+            writer.write(xmlData);
+            writer.write("</Contribuyentes>\n");
+        }catch(Exception e){
+            System.out.println("No se ha podido guardar el xml de errores NifNie");
+            e.printStackTrace();
+            return false;
         }
-        return "NotValid";
+        return true;
     }
+    public static String getXML(Contribuyente cont, String ErrorType) throws IOException{
+        StringBuilder sb = new StringBuilder();
 
+        sb.append("  <Contribuyente id=\"").append(cont.getIdContribuyente()).append("\">\n");
+        sb.append("    <NIF_NIE>").append(cont.getNifnie()).append("</NIF_NIE>\n");
+        sb.append("    <Nombre>").append(cont.getNombre()).append("</Nombre>\n");
+        sb.append("    <PrimerApellido>").append(cont.getApellido1()).append("</PrimerApellido>\n");
+        sb.append("    <SegundoApellido>").append(cont.getApellido2()).append("</SegundoApellido>\n");
+        sb.append("    <TipoDeError>").append(ErrorType).append("</TipoDeError>\n");
+        sb.append("  </Contribuyente>\n");
+
+        return sb.toString();
+    }
 }
